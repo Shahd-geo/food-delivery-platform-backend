4928ffadf24f43551159c41215fe2c975cb56137
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 11:50:59 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/Restaurant.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/Restaurant.java
@@ -5,4 +5,13 @@ import jakarta.persistence.Entity;
 @Entity
 public class Restaurant extends BaseEntity{
 
+    private String name;
+    private String description;
+    private String cuisineType;
+    private String openingTime;
+    private String closingTime;
+    private Double minOrderAmount;
+    private Double deliveryFee;
+    private Boolean acceptingOrders;
 }
+
