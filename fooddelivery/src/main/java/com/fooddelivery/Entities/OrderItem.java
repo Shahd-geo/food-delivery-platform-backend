2557313c9a94127e3c0237e6d24d66c3156a3633
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 12:04:53 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/OrderItem.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/OrderItem.java
@@ -1,4 +1,18 @@
 package com.fooddelivery.Entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class OrderItem extends BaseEntity{
+    private Integer quantity;
+    private Double unitPrice;
+    private Double totalPrice;
+    private String specialInstructions;
 }
