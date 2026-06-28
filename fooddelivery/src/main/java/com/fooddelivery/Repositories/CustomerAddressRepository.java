88ab163201dab915c56e86ff1da3ead23fa31345
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 21:19:50 +0400

    CustomerAddressRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/CustomerAddressRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/CustomerAddressRepository.java
@@ -2,6 +2,17 @@ package com.fooddelivery.Repositories;
 
 import com.fooddelivery.Entities.CustomerAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, Integer> {{
+import java.util.List;
+
+public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, Integer> {
+    @Query("""
+       SELECT ca
+       FROM CustomerAddress ca
+       WHERE ca.city = :city
+       AND ca.isActive = true
+       """)
+    List<CustomerAddress> findByCity(@Param("city") String city);
 }
