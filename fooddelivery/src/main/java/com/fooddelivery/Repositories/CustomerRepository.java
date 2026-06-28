401e571e77c1d65f215f5492482d06a26b30820c
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 21:15:15 +0400

    add quaery in CustomerRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/CustomerRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/CustomerRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,4 +19,20 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
        AND c.isActive = true
        """)
     Optional<Customer> findByEmail(@Param("email") String email);
+
+    @Query("""
+       SELECT c
+       FROM Customer c
+       WHERE c.loyaltyPoints >= :points
+       AND c.isActive = true
+       """)
+    List<Customer> findByLoyaltyPointsGreaterThanEqual(@Param("points") Integer points);
+
+    @Query("""
+       SELECT c
+       FROM Customer c
+       WHERE c.createdDate BETWEEN :startDate AND :endDate
+       AND c.isActive = true
+       """)
+    List<Customer> findCustomersRegisteredBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
