bfcd9f4a2f30fe4552e14fe2fae4ff388f9dd7f9
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 21:23:16 +0400

    create RestaurantRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/RestaurantRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/RestaurantRepository.java
@@ -1,4 +1,10 @@
 package com.fooddelivery.Repositories;
 
-public interface RestaurantRepository {
+import com.fooddelivery.Entities.Customer;
+import com.fooddelivery.Entities.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository  extends JpaRepository<Restaurant, Integer> {
 }
