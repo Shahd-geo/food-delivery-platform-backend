96eb4fff66cca8baffdb134d75ff49611c0ca719
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 11:46:52 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/RestaurantOwner.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/RestaurantOwner.java
@@ -1,4 +1,13 @@
 package com.fooddelivery.Entities;
 
+import jakarta.validation.constraints.Email;
+
 public class RestaurantOwner extends BaseEntity {
+    private String firstName;
+    private String lastName;
+@Email
+    private String email;
+    private String phone;
+    private String passwordHash;
+    private String businessLicenseCode;
 }
