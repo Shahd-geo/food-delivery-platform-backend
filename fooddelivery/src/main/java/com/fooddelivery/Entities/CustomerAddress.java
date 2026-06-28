f57cd87df6c05e9d6f6b82e7eb821420683ed86e
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 11:37:17 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/CustomerAddress.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/CustomerAddress.java
@@ -1,4 +1,17 @@
 package com.fooddelivery.Entities;
 
-public class CustomerAddress {
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerAddress extends BaseEntity{
+    private String street;
+    private String city;
+    private String building;
+    private Boolean isDefault;
 }
