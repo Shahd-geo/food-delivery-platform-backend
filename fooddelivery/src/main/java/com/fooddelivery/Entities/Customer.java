021d1b27cc368ebde6e95a322c481714b0f453e3
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 11:21:17 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/Customer.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/Customer.java
@@ -1,7 +1,22 @@
 package com.fooddelivery.Entities;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.UniqueElements;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer extends BaseEntity{
+    private String firstName;
+    private String lastName;
+    @UniqueElements
+    private String email;
+    private String phone;
+    private String passwordHash;
+    private Integer loyaltyPoints;
+    private String customerCode;
 }
