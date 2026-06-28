a3f9d4983b6a5b55d067586fde8a3bb834961803
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 11:43:06 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/DeliveryDriver.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/DeliveryDriver.java
@@ -1,4 +1,24 @@
 package com.fooddelivery.Entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DeliveryDriver extends BaseEntity{
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String passwordHash;
+    private String driverCode;
+    private String vehicleType;
+    private String vehiclePlate;
+    private Double currentLat;
+    private Double currentLng;
+    private Boolean isOnline;
 }
