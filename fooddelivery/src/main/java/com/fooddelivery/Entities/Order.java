032b9af01d25448b2f0ac1892848bab60b118cd6
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 12:00:12 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/Order.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/Order.java
@@ -1,4 +1,23 @@
 package com.fooddelivery.Entities;
 
-public class Order {
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order extends  BaseEntity {
+    private String orderCode;
+    private LocalDateTime orderDate;
+    private String status;
+    private Double subtotal;
+    private Double deliveryFee;
+    private Double discountAmount;
+    private Double totalAmount;
+    private String deliveryNotes;
 }
