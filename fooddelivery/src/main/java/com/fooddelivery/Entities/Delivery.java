dc2fd3133c9027afc8c2beb3c9ffe411f8c88c10
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 12:09:50 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/Delivery.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/Delivery.java
@@ -1,5 +1,19 @@
 package com.fooddelivery.Entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Delivery extends BaseEntity{
+    private String trackingCode;
+    private String status;
+    private String assignedAt;
+    private String pickedUpAt;
+    private String deliveredAt;
 
 }
