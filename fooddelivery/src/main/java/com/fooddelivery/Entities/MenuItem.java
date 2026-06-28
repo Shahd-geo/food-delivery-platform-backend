a649bbc9ee9f7ee2677ea2fe43d6bf160697aeb6
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 11:54:53 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/MenuItem.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/MenuItem.java
@@ -1,7 +1,19 @@
 package com.fooddelivery.Entities;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MenuItem extends BaseEntity {
+    private String name;
+    private String description;
+    private Double price;
+    private Boolean isAvailable;
+    private Boolean isVegetarian;
+    private Integer calories;
 }
