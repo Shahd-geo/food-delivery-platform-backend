6969871270ba0d968d08fd508ee3d28f922bc90d
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 12:13:51 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/Review.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/Review.java
@@ -1,4 +1,17 @@
 package com.fooddelivery.Entities;
 
-public class Review {
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review extends BaseEntity{
+    private String targetType;
+    private Integer rating;
+    private String comment;
+    private String createdAt;
 }
