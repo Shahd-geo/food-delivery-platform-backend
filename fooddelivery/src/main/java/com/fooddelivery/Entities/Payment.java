969cb395ed55c55db4e6f0202db93f4d06ed0558
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 12:10:50 +0400

    CREATE Payment CLASS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/Payment.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/Payment.java
@@ -1,4 +1,13 @@
 package com.fooddelivery.Entities;
 
-public class Payment {
-}
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment extends BaseEntity{
+        }
