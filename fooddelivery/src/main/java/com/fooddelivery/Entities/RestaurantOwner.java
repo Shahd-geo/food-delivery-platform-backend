3b56255d0b53bce07e3ef23f43f27a8af0624ced
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 11:49:00 +0400

    USING LAMBOK

--- fooddelivery/src/main/java/com/fooddelivery/Entities/RestaurantOwner.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/RestaurantOwner.java
@@ -1,7 +1,15 @@
 package com.fooddelivery.Entities;
 
+import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RestaurantOwner extends BaseEntity {
     private String firstName;
     private String lastName;
