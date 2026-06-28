e78e282d9e409c2a561a58cc4d52195846cc347a
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 22:08:30 +0400

    create ComboMealRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/ComboMealRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/ComboMealRepository.java
@@ -2,8 +2,28 @@ package com.fooddelivery.Repositories;
 
 import com.fooddelivery.Entities.ComboMeal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ComboMealRepository extends JpaRepository<ComboMeal, Integer> {
+    @Query("""
+       SELECT c
+       FROM ComboMeal c
+       WHERE c.restaurant.id = :restaurantId
+       AND c.isActive = true
+       """)
+    List<ComboMeal> findByRestaurantId(@Param("restaurantId") Integer restaurantId);
+
+    @Query("""
+       SELECT c
+       FROM ComboMeal c
+       JOIN c.menuItems m
+       WHERE m.id = :menuItemId
+       AND c.isActive = true
+       """)
+    List<ComboMeal> findByMenuItemId(@Param("menuItemId") Integer menuItemId);
 }
