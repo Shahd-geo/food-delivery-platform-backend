7c091ae9b0772d351870dffefde73e802167fe6e
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 11:07:53 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/BaseEntity.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/BaseEntity.java
@@ -3,10 +3,17 @@ package com.fooddelivery.Entities;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@MappedSuperclass
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String createdDate;
     private String updatedDate;
