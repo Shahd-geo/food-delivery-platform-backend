9aa3567dc9f99f69b905fec7e9f589409ac542e5
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 21:00:50 +0400

    add quaery in CustomerRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/CustomerRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/CustomerRepository.java
@@ -2,8 +2,19 @@ package com.fooddelivery.Repositories;
 
 import com.fooddelivery.Entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    @Query("""
+       SELECT c
+       FROM Customer c
+       WHERE c.email = :email
+       AND c.isActive = true
+       """)
+    Optional<Customer> findByEmail(@Param("email") String email);
 }
