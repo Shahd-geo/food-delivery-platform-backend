b075c86faffbf34754b1d77567615b824048b718
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 21:58:44 +0400

    create ComboMealRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/ComboMealRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/ComboMealRepository.java
@@ -1,4 +1,9 @@
 package com.fooddelivery.Repositories;
 
-public interface ComboMealRepository {
+import com.fooddelivery.Entities.ComboMeal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ComboMealRepository extends JpaRepository<ComboMeal, Integer> {
 }
