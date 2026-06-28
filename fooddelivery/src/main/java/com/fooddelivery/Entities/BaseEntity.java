7507833137f5943b407c9317ff350fb4cc0f1dce
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 11:05:16 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/BaseEntity.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/BaseEntity.java
@@ -1,4 +1,14 @@
 package com.fooddelivery.Entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String createdDate;
+    private String updatedDate;
+    private Boolean isActive;
 }
