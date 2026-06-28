3b56bf63406db417b020e14518eb1efb475f83e1
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-28 12:02:37 +0400

    ADD FIELDS

--- fooddelivery/src/main/java/com/fooddelivery/Entities/CorporateOrder.java
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/CorporateOrder.java
@@ -1,4 +1,21 @@
 package com.fooddelivery.Entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CorporateOrder extends BaseEntity{
+    private String corporateCode;
+    private String companyName;
+    private String costCenter;
+    private LocalDateTime orderDate;
+    private String status;
+    private Double totalAmount;
 }
