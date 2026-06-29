e422b260d2d4ac996401549bb3f9f42c0d4374e3
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 11:59:58 +0400

    CREATE ReviewResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/ReviewResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/ReviewResponseDTO.java
@@ -1,4 +1,9 @@
 package com.fooddelivery.Dto;
 
 public class ReviewResponseDTO {
+    private Integer id;
+    private String targetType;
+    private Integer rating;
+    private String comment;
+    private String createdAt;
 }
