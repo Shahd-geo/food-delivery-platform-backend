21296e34e22d4c35ded510357cb628f6e4a60324
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 11:39:46 +0400

    CREATE OrderResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/OrderResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/OrderResponseDTO.java
@@ -1,4 +1,25 @@
 package com.fooddelivery.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderResponseDTO {
+    private Integer id;
+    private String orderCode;
+    private LocalDateTime orderDate;
+    private String status;
+    private Double subtotal;
+    private Double deliveryFee;
+    private Double discountAmount;
+    private Double totalAmount;
+    private String deliveryNotes;
+    private CustomerSummaryDTO customer;
+    private RestaurantSummaryDTO restaurant;
+    private List<OrderItemResponseDTO> orderItems;
 }
