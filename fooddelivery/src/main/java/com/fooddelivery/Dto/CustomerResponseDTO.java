4ec3d4937e5623a0066a2f105bdf64877bd988df
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 09:55:31 +0400

    CREATE CustomerRequestDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/CustomerResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/CustomerResponseDTO.java
@@ -1,4 +1,46 @@
 package com.fooddelivery.Dto;
 
+import com.fooddelivery.Entities.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerResponseDTO {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Integer loyaltyPoints;
+    private String customerCode;
+
+    //IF I have one customer
+   public  static CustomerResponseDTO fromEntity(Customer customer) {
+        CustomerResponseDTO dto = new CustomerResponseDTO();
+        dto.setId(customer.getId());
+        dto.setFirstName(customer.getFirstName());
+        dto.setLastName(customer.getLastName());
+        dto.setEmail(customer.getEmail());
+        dto.setPhone(customer.getPhone());
+        dto.setLoyaltyPoints(customer.getLoyaltyPoints());
+        dto.setCustomerCode(customer.getCustomerCode());
+
+        return dto;
+    }
+    //IF I HAVE A LIST OF CUSTOMER
+    public static List<CustomerResponseDTO> fromEntity(List<Customer> customers) {
+        List<CustomerResponseDTO> dtos = new ArrayList<>();
+
+        for (Customer customer : customers) {
+            dtos.add(fromEntity(customer));
+        }
+        return dtos;
+    }
+
 }
