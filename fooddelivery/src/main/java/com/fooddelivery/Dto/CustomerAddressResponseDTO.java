9cb3d2cd851d88980232b3c5b49acde446642113
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 10:14:33 +0400

    CREATE CustomerAddressResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/CustomerAddressResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/CustomerAddressResponseDTO.java
@@ -1,4 +1,16 @@
 package com.fooddelivery.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerAddressResponseDTO {
+    private Integer id;
+    private String street;
+    private String city;
+    private String building;
+    private Boolean isDefault;
 }
