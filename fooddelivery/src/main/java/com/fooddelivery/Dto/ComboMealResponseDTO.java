d0acc45a3e0e1d784ede015032f7643b52386d4b
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 10:53:25 +0400

    CREATE ComboMealResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/ComboMealResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/ComboMealResponseDTO.java
@@ -1,4 +1,9 @@
 package com.fooddelivery.Dto;
 
 public class ComboMealResponseDTO {
+    private Integer id;
+    private String comboName;
+    private String description;
+    private Double totalPrice;
+    private Boolean isAvailable;
 }
