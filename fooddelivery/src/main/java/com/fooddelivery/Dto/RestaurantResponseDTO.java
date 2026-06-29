b3f201b2e576f281fb9dbbd97c7507c15dc53020
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 10:38:49 +0400

    CREATE RestaurantResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/RestaurantResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/RestaurantResponseDTO.java
@@ -1,5 +1,6 @@
 package com.fooddelivery.Dto;
 
+import com.fooddelivery.Entities.Restaurant;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,4 +18,20 @@ public class RestaurantResponseDTO {
     private Double minOrderAmount;
     private Double deliveryFee;
     private Boolean acceptingOrders;
+
+    // IF ONE RESTURENT
+    public static RestaurantResponseDTO fromEntity(Restaurant restaurant) {
+        RestaurantResponseDTO dto = new RestaurantResponseDTO();
+        dto.setId(restaurant.getId());
+        dto.setName(restaurant.getName());
+        dto.setDescription(restaurant.getDescription());
+        dto.setCuisineType(restaurant.getCuisineType());
+        dto.setOpeningTime(restaurant.getOpeningTime());
+        dto.setClosingTime(restaurant.getClosingTime());
+        dto.setMinOrderAmount(restaurant.getMinOrderAmount());
+        dto.setDeliveryFee(restaurant.getDeliveryFee());
+        dto.setAcceptingOrders(restaurant.getAcceptingOrders());
+
+        return dto;
+    }
 }
