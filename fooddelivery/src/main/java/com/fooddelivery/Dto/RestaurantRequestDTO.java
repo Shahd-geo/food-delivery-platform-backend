160961be07ab2aef262312c660ad5b39ab2c4a91
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 10:34:39 +0400

    CREATE RestaurantRequestDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/RestaurantRequestDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/RestaurantRequestDTO.java
@@ -1,4 +1,25 @@
 package com.fooddelivery.Dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
 public class RestaurantRequestDTO {
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotBlank
+    private String cuisineType;
+    @NotBlank
+    private String openingTime;
+    @NotBlank
+    private String closingTime;
+    @PositiveOrZero
+    private Double minOrderAmount;
+    @PositiveOrZero
+    private Double deliveryFee;
+    @NotNull
+    private Boolean acceptingOrders;
+
 }
