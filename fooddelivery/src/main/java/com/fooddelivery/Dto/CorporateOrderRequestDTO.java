f38f2769fdf75fe07932f84f6c11b8f896dd5486
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 12:21:27 +0400

    CREATE CorporateOrderRequestDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/CorporateOrderRequestDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/CorporateOrderRequestDTO.java
@@ -1,5 +1,6 @@
 package com.fooddelivery.Dto;
 
+import com.fooddelivery.Entities.CorporateOrder;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -18,4 +19,16 @@ public class CorporateOrderRequestDTO {
     private Double totalAmount;
     @NotNull
     private Integer restaurantId;
+
+    public CorporateOrder toEntity() {
+
+        CorporateOrder corporateOrder = new CorporateOrder();
+        corporateOrder.setCorporateCode(this.corporateCode);
+        corporateOrder.setCompanyName(this.companyName);
+        corporateOrder.setCostCenter(this.costCenter);
+        corporateOrder.setStatus(this.status);
+        corporateOrder.setTotalAmount(this.totalAmount);
+
+        return corporateOrder;
+    }
 }
