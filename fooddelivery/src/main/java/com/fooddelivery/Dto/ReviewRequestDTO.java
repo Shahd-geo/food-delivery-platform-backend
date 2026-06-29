57c433e53d0317c8deeffe5933736a2520297470
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 11:57:57 +0400

    CREATE ReviewRequestDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/ReviewRequestDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/ReviewRequestDTO.java
@@ -1,4 +1,17 @@
 package com.fooddelivery.Dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class ReviewRequestDTO {
+    @NotBlank
+    private String targetType;
+    @NotNull
+    @Min(1)
+    @Max(5)
+    private Integer rating;
+    @NotBlank
+    private String comment;
 }
