d42f7e9b7f24953b97609118c997c599a1f30da2
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 11:07:36 +0400

    CREATE OrderItemResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/OrderItemResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/OrderItemResponseDTO.java
@@ -1,5 +1,6 @@
 package com.fooddelivery.Dto;
 
+import com.fooddelivery.Entities.OrderItem;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,15 @@ public class OrderItemResponseDTO {
     private Double unitPrice;
     private Double totalPrice;
     private String specialInstructions;
+
+    public static OrderItemResponseDTO fromEntity(OrderItem orderItem) {
+        OrderItemResponseDTO dto = new OrderItemResponseDTO();
+        dto.setId(orderItem.getId());
+        dto.setQuantity(orderItem.getQuantity());
+        dto.setUnitPrice(orderItem.getUnitPrice());
+        dto.setTotalPrice(orderItem.getTotalPrice());
+        dto.setSpecialInstructions(orderItem.getSpecialInstructions());
+
+        return dto;
+    }
 }
