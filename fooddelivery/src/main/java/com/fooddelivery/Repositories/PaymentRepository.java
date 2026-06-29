38254428d0ceddf451a169a548e43bf7e15d4ce2
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 08:59:36 +0400

    create PaymentRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/PaymentRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/PaymentRepository.java
@@ -2,6 +2,8 @@ package com.fooddelivery.Repositories;
 
 import com.fooddelivery.Entities.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaymentRepository  extends JpaRepository<Payment,Integer> {
 }
