8ab33968f3f3df6e94d432891f8a83dd344be36b
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 09:30:48 +0400

    CREATE CustomerRequestDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/CustomerRequestDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/CustomerRequestDTO.java
@@ -1,4 +1,20 @@
 package com.fooddelivery.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public class CustomerRequestDTO {
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    @Email
+    private String email;
+    @Pattern(regexp = "^\\+?[0-9]{8,15}$")
+    private String phone;
+    @NotBlank
+    private String passwordHash;
+
 }
