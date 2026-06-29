ff1164b281fb8094c2628cd5cd5aeeff8e1ffd53
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 10:54:33 +0400

    CREATE ComboMealResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/ComboMealResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/ComboMealResponseDTO.java
@@ -1,5 +1,6 @@
 package com.fooddelivery.Dto;
 
+import com.fooddelivery.Entities.ComboMeal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,15 @@ public class ComboMealResponseDTO {
     private String description;
     private Double totalPrice;
     private Boolean isAvailable;
+
+    public static ComboMealResponseDTO fromEntity(ComboMeal comboMeal) {
+        ComboMealResponseDTO dto = new ComboMealResponseDTO();
+        dto.setId(comboMeal.getId());
+        dto.setComboName(comboMeal.getComboName());
+        dto.setDescription(comboMeal.getDescription());
+        dto.setTotalPrice(comboMeal.getTotalPrice());
+        dto.setIsAvailable(comboMeal.getIsAvailable());
+
+        return dto;
+    }
 }
