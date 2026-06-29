8e4fda7a092ba094304cf4ab53731d749c20a643
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 12:18:26 +0400

    CREATE RestaurantOwnerResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/RestaurantOwnerResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/RestaurantOwnerResponseDTO.java
@@ -1,5 +1,6 @@
 package com.fooddelivery.Dto;
 
+import com.fooddelivery.Entities.RestaurantOwner;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,19 @@ public class RestaurantOwnerResponseDTO {
     private String lastName;
     private String email;
     private String phone;
-    private String businessLicenseCod
+    private String businessLicenseCode;
+
+    public static RestaurantOwnerResponseDTO fromEntity(RestaurantOwner owner) {
+
+        RestaurantOwnerResponseDTO dto = new RestaurantOwnerResponseDTO();
+
+        dto.setId(owner.getId());
+        dto.setFirstName(owner.getFirstName());
+        dto.setLastName(owner.getLastName());
+        dto.setEmail(owner.getEmail());
+        dto.setPhone(owner.getPhone());
+        dto.setBusinessLicenseCode(owner.getBusinessLicenseCode());
+
+        return dto;
+    }
 }
