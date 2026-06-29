6b668ed0aa2c33fc2adf38e43b66a584d1a4529b
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 12:04:07 +0400

    CREATE PaymentRequestDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/PaymentRequestDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/PaymentRequestDTO.java
@@ -5,7 +5,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentRequestDTO {
    @NotBlank
     private String paymentMethod;
