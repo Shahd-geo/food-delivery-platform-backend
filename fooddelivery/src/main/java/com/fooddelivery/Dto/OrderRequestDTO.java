a0660e9de98bbd660df4b9b818efea40e6e45291
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 11:20:30 +0400

    CREATE OrderRequestDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/OrderRequestDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/OrderRequestDTO.java
@@ -1,4 +1,31 @@
 package com.fooddelivery.Dto;
 
+import com.fooddelivery.Entities.Order;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+import java.util.List;
+
 public class OrderRequestDTO {
+    @NotNull
+    private Integer customerId;
+    @NotNull
+    private Integer restaurantId;
+
+    @Pattern(regexp = "PENDING|CONFIRMED|PREPARING|OUT_FOR_DELIVERY|DELIVERED|CANCELLED")
+    private String status;
+    private String deliveryNotes;
+    @NotEmpty
+    @Valid
+    private List<OrderItemRequestDTO> orderItems;
+
+    public Order toEntity() {
+        Order order = new Order();
+        order.setStatus(this.status);
+        order.setDeliveryNotes(this.deliveryNotes);
+
+        return order;
+    }
 }
