613b275dfb19c84f77f983b7260fb420e22a7b8b
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 08:58:19 +0400

    create DeliveryRepository quary

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/CustomerAddressRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/CustomerAddressRepository.java
@@ -4,9 +4,10 @@ import com.fooddelivery.Entities.CustomerAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, Integer> {
     @Query("""
        SELECT ca
--- fooddelivery/src/main/java/com/fooddelivery/Repositories/OrderRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/OrderRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
     @Query("""
        SELECT o
