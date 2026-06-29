839314afc20de4700b4337c179223d3382472c3d
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 11:03:07 +0400

    CREATE OrderItemRequestDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/OrderItemRequestDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/OrderItemRequestDTO.java
@@ -3,7 +3,13 @@ package com.fooddelivery.Dto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemRequestDTO {
     @Positive
     private Integer quantity;
