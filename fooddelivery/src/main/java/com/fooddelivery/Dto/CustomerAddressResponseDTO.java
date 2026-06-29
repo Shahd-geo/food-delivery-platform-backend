8de45ce47c323047ea05531c61f963844845173f
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 10:15:58 +0400

    CREATE CustomerAddressResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/CustomerAddressResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/CustomerAddressResponseDTO.java
@@ -1,5 +1,6 @@
 package com.fooddelivery.Dto;
 
+import com.fooddelivery.Entities.CustomerAddress;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,14 @@ public class CustomerAddressResponseDTO {
     private String city;
     private String building;
     private Boolean isDefault;
+
+    public static CustomerAddressResponseDTO fromEntity(CustomerAddress address) {
+        CustomerAddressResponseDTO dto = new CustomerAddressResponseDTO();
+        dto.setId(address.getId());
+        dto.setStreet(address.getStreet());
+        dto.setCity(address.getCity());
+        dto.setBuilding(address.getBuilding());
+        dto.setIsDefault(address.getIsDefault());
+        return dto;
+    }
 }
