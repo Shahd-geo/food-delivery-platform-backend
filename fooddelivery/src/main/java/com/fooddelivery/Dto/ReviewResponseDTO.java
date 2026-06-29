32d45e3d5d443866bcf7f96d657449fce36f3e7b
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 12:01:06 +0400

    CREATE ReviewResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/ReviewResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/ReviewResponseDTO.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -24,6 +26,17 @@ public class ReviewResponseDTO {
         dto.setCreatedAt(review.getCreatedAt());
 
         return dto;
+    }
+
+    public static List<ReviewResponseDTO> fromEntity(List<Review> reviews) {
+
+        List<ReviewResponseDTO> dtos = new ArrayList<>();
+
+        for (Review review : reviews) {
+            dtos.add(fromEntity(review));
+        }
+
+        return dtos;
     }
 
 }
