307841709ce322e34667eda3c0a1314b68423740
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 10:09:16 +0400

    CREATE CustomerAddressRequestDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/CustomerAddressRequestDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/CustomerAddressRequestDTO.java
@@ -2,8 +2,13 @@ package com.fooddelivery.Dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerAddressRequestDTO {
     @NotBlank
     private String street;
