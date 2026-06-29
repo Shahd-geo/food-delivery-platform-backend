b8f5870f8527b8360ac0f2efc82792ed5d76fe49
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 11:52:00 +0400

    CREATE OrderSummaryDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/OrderSummaryDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/OrderSummaryDTO.java
@@ -1,4 +1,9 @@
 package com.fooddelivery.Dto;
 
 public class OrderSummaryDTO {
+    private Integer id;
+    private String orderCode;
+    private String status;
+    private Double totalAmount;
+
 }
