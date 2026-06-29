db8b8e16e532664eb5aaa4688ac7df7c597f1e3d
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 11:38:20 +0400

    CREATE RestaurantSummaryDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/RestaurantSummaryDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/RestaurantSummaryDTO.java
@@ -1,4 +1,23 @@
 package com.fooddelivery.Dto;
 
+import com.fooddelivery.Entities.Restaurant;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RestaurantSummaryDTO {
+    private Integer id;
+    private String name;
+    public static RestaurantSummaryDTO fromEntity(Restaurant restaurant) {
+
+        RestaurantSummaryDTO dto = new RestaurantSummaryDTO();
+
+        dto.setId(restaurant.getId());
+        dto.setName(restaurant.getName());
+
+        return dto;
+    }
 }
