6715d91bbd87014545ba3abccce3316982b0200e
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 12:22:34 +0400

    CREATE CorporateOrderResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/CorporateOrderResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/CorporateOrderResponseDTO.java
@@ -1,4 +1,14 @@
 package com.fooddelivery.Dto;
 
+import java.time.LocalDateTime;
+
 public class CorporateOrderResponseDTO {
+    private Integer id;
+    private String corporateCode;
+    private String companyName;
+    private String costCenter;
+    private LocalDateTime orderDate;
+    private String status;
+    private Double totalAmount;
+    private RestaurantSummaryDTO restaurant;
 }
