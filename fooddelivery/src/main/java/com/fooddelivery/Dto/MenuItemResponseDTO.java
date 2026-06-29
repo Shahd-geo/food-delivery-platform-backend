3550d63431f251a34b917ad8d4fc40144fc54f9f
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 10:47:06 +0400

    CREATE MenuItemResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/MenuItemResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/MenuItemResponseDTO.java
@@ -1,4 +1,11 @@
 package com.fooddelivery.Dto;
 
 public class MenuItemResponseDTO {
+    private Integer id;
+    private String name;
+    private String description;
+    private Double price;
+    private Boolean isAvailable;
+    private Boolean isVegetarian;
+    private Integer calories;
 }
