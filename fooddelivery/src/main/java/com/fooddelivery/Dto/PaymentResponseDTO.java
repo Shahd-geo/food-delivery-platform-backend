999c5cfe061ec6c25bc6e92c2c6859dc6b1d4146
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 12:04:51 +0400

    CREATE PaymentResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/PaymentResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/PaymentResponseDTO.java
@@ -1,4 +1,11 @@
 package com.fooddelivery.Dto;
 
 public class PaymentResponseDTO {
+    private Integer id;
+    private String paymentMethod;
+    private String status;
+    private Double amount;
+    private String transactionRef;
+    private String processedAt;
+    private OrderSummaryDTO order;
 }
