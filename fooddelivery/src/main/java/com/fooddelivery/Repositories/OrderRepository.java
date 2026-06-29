4a2853f60499e7c67077aeb2ba2c7a0f36ec6b94
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 08:09:03 +0400

    create OrderRepository quary

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/OrderRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/OrderRepository.java
@@ -1,7 +1,49 @@
 package com.fooddelivery.Repositories;
 
 import com.fooddelivery.Entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface OrderRepository extends CrudRepository<Order, Integer> {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+    @Query("""
+       SELECT o
+       FROM Order o
+       WHERE o.customer.id = :customerId
+       AND o.isActive = true
+       """)
+    List<Order> findByCustomerId(@Param("customerId") Integer customerId);
+
+
+    @Query("""
+       SELECT o
+       FROM Order o
+       WHERE o.restaurant.id = :restaurantId
+       AND o.status = :status
+       AND o.isActive = true
+       """)
+    List<Order> findByRestaurantIdAndStatus(@Param("restaurantId") Integer restaurantId, @Param("status") String status);
+
+    @Query("""
+       SELECT o
+       FROM Order o
+       WHERE o.orderDate BETWEEN :startDate AND :endDate
+       AND o.isActive = true
+       """)
+    List<Order> findByOrderDateBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+
+
+    //Count Completed Orders
+    @Query("""
+       SELECT COUNT(o)
+       FROM Order o
+       WHERE o.restaurant.id = :restaurantId
+       AND o.status = 'DELIVERED'
+       AND o.isActive = true
+       """)
+    Long countCompletedOrdersForRestaurant(@Param("restaurantId") Integer restaurantId);
 }
