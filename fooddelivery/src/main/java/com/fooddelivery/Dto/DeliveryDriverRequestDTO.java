ce9d23a3afe00c5e55313df7ae9e8ac595ab1eb8
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 12:09:34 +0400

    CREATE DeliveryDriverRequestDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/DeliveryDriverRequestDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/DeliveryDriverRequestDTO.java
@@ -1,4 +1,32 @@
 package com.fooddelivery.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public class DeliveryDriverRequestDTO {
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    @Email
+    private String email;
+    @Pattern(regexp = "^(\\+968)?[0-9]{8}$")
+    private String phone;
+    @NotBlank
+    private String passwordHash;
+    @NotBlank
+    private String driverCode;
+    @NotBlank
+    private String vehicleType;
+    @NotBlank
+    private String vehiclePlate;
+    @NotNull
+    private Double currentLat;
+    @NotNull
+    private Double currentLng;
+    @NotNull
+    private Boolean isOnline;
 }
