5033df987d7e34b793db752003c6f0bc00bd3b88
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 11:06:50 +0400

    CREATE OrderItemResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/OrderItemResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/OrderItemResponseDTO.java
@@ -1,4 +1,9 @@
 package com.fooddelivery.Dto;
 
 public class OrderItemResponseDTO {
+    private Integer id;
+    private Integer quantity;
+    private Double unitPrice;
+    private Double totalPrice;
+    private String specialInstructions;
 }
