7a531e441a5b5d247f4be560c0e91e2605fb28fa
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 12:16:26 +0400

    CREATE RestaurantOwnerResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/RestaurantOwnerResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/RestaurantOwnerResponseDTO.java
@@ -1,4 +1,10 @@
 package com.fooddelivery.Dto;
 
 public class RestaurantOwnerResponseDTO {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String businessLicenseCod
 }
