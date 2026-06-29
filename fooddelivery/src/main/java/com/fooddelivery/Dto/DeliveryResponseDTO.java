9ad057c5c65a4a50069ce199f9818d8f19607f61
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 11:48:29 +0400

    CREATE DeliveryResponseDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/DeliveryResponseDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/DeliveryResponseDTO.java
@@ -1,5 +1,12 @@
 package com.fooddelivery.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DeliveryResponseDTO {
     private Integer id;
     private String trackingCode;
