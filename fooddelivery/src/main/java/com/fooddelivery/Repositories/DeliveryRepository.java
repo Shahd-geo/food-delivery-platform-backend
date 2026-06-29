8ea78dccd65a5735e01db46f9871452d4e917274
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 08:51:29 +0400

    create DeliveryRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/DeliveryRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/DeliveryRepository.java
@@ -1,6 +1,7 @@
 package com.fooddelivery.Repositories;
 
 import com.fooddelivery.Entities.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DeliveryRepository  extends JpaRepository<Delivery,Integer>{
+public interface DeliveryRepository  extends JpaRepository<Delivery,Integer> {
 }
