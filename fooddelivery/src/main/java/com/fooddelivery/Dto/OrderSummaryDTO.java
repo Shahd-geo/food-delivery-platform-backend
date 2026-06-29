ad0092bbbc9d4c64ffaec2e9cfc98cf679d85c41
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-29 11:52:40 +0400

    CREATE OrderSummaryDTO

--- fooddelivery/src/main/java/com/fooddelivery/Dto/OrderSummaryDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/Dto/OrderSummaryDTO.java
@@ -1,9 +1,26 @@
 package com.fooddelivery.Dto;
 
+import com.fooddelivery.Entities.Order;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderSummaryDTO {
     private Integer id;
     private String orderCode;
     private String status;
     private Double totalAmount;
 
+    public static OrderSummaryDTO fromEntity(Order order) {
+        OrderSummaryDTO dto = new OrderSummaryDTO();
+        dto.setId(order.getId());
+        dto.setOrderCode(order.getOrderCode());
+        dto.setStatus(order.getStatus());
+        dto.setTotalAmount(order.getTotalAmount());
+
+        return dto;
+    }
 }
