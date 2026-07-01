0dae0a9d90fafc8ffdc833e02d034eb693b32d6c
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-01 14:27:21 +0400

    CREATE RestaurantOwnerRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/RestaurantOwnerRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/RestaurantOwnerRepository.java
@@ -1,4 +1,10 @@
 package com.fooddelivery.Repositories;
 
-public interface RestaurantOwnerRepository {
+import com.fooddelivery.Entities.RestaurantOwner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantOwnerRepository  extends JpaRepository<RestaurantOwner, Integer> {
 }
