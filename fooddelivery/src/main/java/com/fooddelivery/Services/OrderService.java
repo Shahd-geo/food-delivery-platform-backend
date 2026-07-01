3c5279471b1e31e5548facd0bd2fc7e1ebd471b4
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-01 15:11:36 +0400

    @Autowired the reposrty

--- fooddelivery/src/main/java/com/fooddelivery/Services/OrderService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/OrderService.java
@@ -1,7 +1,21 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.Repositories.CustomerRepository;
+import com.fooddelivery.Repositories.OrderItemRepository;
+import com.fooddelivery.Repositories.OrderRepository;
+import com.fooddelivery.Repositories.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
 }
