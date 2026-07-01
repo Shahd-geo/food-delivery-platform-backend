5c176353f5b258e5199e57df6a857ff043f678f9
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-01 09:21:50 +0400

    add ErrorResponse class

---
+++ fooddelivery/src/main/java/com/fooddelivery/Exceptions/InvalidRequestException.java
@@ -0,0 +1,9 @@
+package com.fooddelivery.Exceptions;
+
+public class InvalidRequestException extends RuntimeException
+{
+    public InvalidRequestException(String message)
+    {
+        super(message);
+    }
+}
