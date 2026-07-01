5f789818c201e52d91753b8729ffefe10bf0141b
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-01 15:10:08 +0400

    ؤCREATE OrderItemRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/OrderItemRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/OrderItemRepository.java
@@ -1,7 +1,9 @@
 package com.fooddelivery.Repositories;
 
+import com.fooddelivery.Entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class OrderItemRepository {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
 }
