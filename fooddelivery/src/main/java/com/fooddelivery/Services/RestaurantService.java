8e99dd0ae24bf99395ef1467a9c341bc761a1d1a
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-01 14:30:21 +0400

    @Autowired the reposrty

--- fooddelivery/src/main/java/com/fooddelivery/Services/RestaurantService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/RestaurantService.java
@@ -1,6 +1,7 @@
 package com.fooddelivery.Services;
 
 import com.fooddelivery.Repositories.MenuItemRepository;
+import com.fooddelivery.Repositories.RestaurantOwnerRepository;
 import com.fooddelivery.Repositories.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
