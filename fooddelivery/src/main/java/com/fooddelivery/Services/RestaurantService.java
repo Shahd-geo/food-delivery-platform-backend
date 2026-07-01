c7c040f8d99c01f1b3bea8cfe6a27a4cba7be374
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-01 14:31:55 +0400

    add createRestaurant method

--- fooddelivery/src/main/java/com/fooddelivery/Services/RestaurantService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/RestaurantService.java
@@ -1,5 +1,10 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.Dto.RestaurantRequestDTO;
+import com.fooddelivery.Dto.RestaurantResponseDTO;
+import com.fooddelivery.Entities.Restaurant;
+import com.fooddelivery.Entities.RestaurantOwner;
+import com.fooddelivery.Exceptions.ResourceNotFoundException;
 import com.fooddelivery.Repositories.MenuItemRepository;
 import com.fooddelivery.Repositories.RestaurantOwnerRepository;
 import com.fooddelivery.Repositories.RestaurantRepository;
@@ -15,4 +20,13 @@ public class RestaurantService {
     private RestaurantOwnerRepository restaurantOwnerRepository;
     @Autowired
     private MenuItemRepository menuItemRepository;
+
+    public RestaurantResponseDTO createRestaurant(RestaurantRequestDTO dto, Integer ownerId) {
+        RestaurantOwner owner = restaurantOwnerRepository.findById(ownerId).orElseThrow(() ->
+                new ResourceNotFoundException("Owner not found"));
+        Restaurant restaurant = dto.toEntity();
+        restaurant.setRestaurantOwner(owner);
+        restaurantRepository.save(restaurant);
+        return RestaurantResponseDTO.fromEntity(restaurant);
+    }
 }
