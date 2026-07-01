6bf37cb6c0aa9d0d973f39e1eae93f0e6ad94a4a
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-01 12:14:41 +0400

    CustomerResponseDTO BY ENTERING CustomerRequestDTO AND CustomerAddressRequestDTO

--- fooddelivery/src/main/java/com/fooddelivery/Services/CustomerService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/CustomerService.java
@@ -1,8 +1,10 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.Dto.CustomerAddressRequestDTO;
 import com.fooddelivery.Dto.CustomerRequestDTO;
 import com.fooddelivery.Dto.CustomerResponseDTO;
 import com.fooddelivery.Entities.Customer;
+import com.fooddelivery.Entities.CustomerAddress;
 import com.fooddelivery.Repositories.CustomerAddressRepository;
 import com.fooddelivery.Repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,14 @@ public class CustomerService {
     public CustomerResponseDTO createCustomer(CustomerRequestDTO dto) {
         Customer customer = dto.toEntity();
         customerRepository.save(customer);
+        return CustomerResponseDTO.fromEntity(customer);
+    }
+    public CustomerResponseDTO createCustomer(CustomerRequestDTO dto, CustomerAddressRequestDTO initialAddress) {
+        Customer customer = dto.toEntity();
+        customerRepository.save(customer);
+        CustomerAddress address = initialAddress.toEntity();
+        address.setCustomer(customer);
+        customerAddressRepository.save(address);
         return CustomerResponseDTO.fromEntity(customer);
     }
 
