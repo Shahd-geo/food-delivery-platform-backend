bd0c1d8225986cf47d9bf99b8349baadfc8c87d6
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-01 14:26:28 +0400

    CREATE RestaurantOwnerRepository

--- fooddelivery/src/main/java/com/fooddelivery/Services/RestaurantService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/RestaurantService.java
@@ -1,7 +1,17 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.Repositories.MenuItemRepository;
+import com.fooddelivery.Repositories.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RestaurantService {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+    @Autowired
+    private RestaurantOwnerRepository restaurantOwnerRepository;
+    @Autowired
+    private MenuItemRepository menuItemRepository;
 }
