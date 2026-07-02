5686b0af9a4de0837952ac2ca16ea58a24c440ad
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-02 11:15:20 +0400

    Autowired

--- fooddelivery/src/main/java/com/fooddelivery/Controllers/CustomerController.java
+++ fooddelivery/src/main/java/com/fooddelivery/Controllers/CustomerController.java
@@ -1,9 +1,13 @@
 package com.fooddelivery.Controllers;
 
+import com.fooddelivery.Services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
 }
