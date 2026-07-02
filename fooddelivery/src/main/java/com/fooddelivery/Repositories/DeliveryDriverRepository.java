eea2d7aeb04f54abc88e54c31762bd2512f99874
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-02 08:01:23 +0400

    create DeliveryDriverRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/DeliveryDriverRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/DeliveryDriverRepository.java
@@ -2,6 +2,18 @@ package com.fooddelivery.Repositories;
 
 import com.fooddelivery.Entities.DeliveryDriver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface DeliveryDriverRepository extends JpaRepository<DeliveryDriver, Integer> {
+    @Query("""
+
+            SELECT d
+        FROM DeliveryDriver d
+        WHERE d.isOnline = true
+        AND d.isActive = true
+        """)
+    List<DeliveryDriver> findOnlineDrivers();
 }
--- fooddelivery/src/main/java/com/fooddelivery/Services/DeliveryService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/DeliveryService.java
@@ -1,5 +1,6 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.Repositories.DeliveryDriverRepository;
 import com.fooddelivery.Repositories.DeliveryRepository;
 import com.fooddelivery.Repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
