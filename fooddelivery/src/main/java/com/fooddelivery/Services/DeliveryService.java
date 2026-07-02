87ae6b740f04f669ba5773edb0f1c21547e25b42
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-02 07:55:42 +0400

    create DeliveryDriverRepository

--- fooddelivery/src/main/java/com/fooddelivery/Services/DeliveryService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/DeliveryService.java
@@ -1,7 +1,18 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.Repositories.DeliveryRepository;
+import com.fooddelivery.Repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DeliveryService {
+    @Autowired
+    private DeliveryRepository deliveryRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private DeliveryDriverRepository deliveryDriverRepository;
 }
