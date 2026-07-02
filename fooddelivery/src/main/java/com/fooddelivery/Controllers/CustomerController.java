3fee3266fb21c4936da106a8ab52b99a7b492bcd
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-02 11:31:37 +0400

    createCustomer

--- fooddelivery/src/main/java/com/fooddelivery/Controllers/CustomerController.java
+++ fooddelivery/src/main/java/com/fooddelivery/Controllers/CustomerController.java
@@ -1,7 +1,14 @@
 package com.fooddelivery.Controllers;
 
+import com.fooddelivery.Dto.CustomerRequestDTO;
+import com.fooddelivery.Dto.CustomerResponseDTO;
 import com.fooddelivery.Services.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
+
+    //Create new customer
+    @PostMapping
+    public ResponseEntity<CustomerResponseDTO> createCustomer(@RequestBody @Valid CustomerRequestDTO dto) {
+        CustomerResponseDTO response = customerService.createCustomer(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
