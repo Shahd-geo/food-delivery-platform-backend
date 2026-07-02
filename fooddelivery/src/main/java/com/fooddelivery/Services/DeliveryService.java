d21bda35be9e67d03ef285e76efc052cbd68b7c7
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-02 08:03:13 +0400

    assignDriverToOrder

--- fooddelivery/src/main/java/com/fooddelivery/Services/DeliveryService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/DeliveryService.java
@@ -1,8 +1,14 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.Dto.DeliveryResponseDTO;
+import com.fooddelivery.Entities.Delivery;
+import com.fooddelivery.Entities.DeliveryDriver;
+import com.fooddelivery.Entities.Order;
+import com.fooddelivery.Exceptions.ResourceNotFoundException;
 import com.fooddelivery.Repositories.DeliveryDriverRepository;
 import com.fooddelivery.Repositories.DeliveryRepository;
 import com.fooddelivery.Repositories.OrderRepository;
+import com.fooddelivery.Utils.HelperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +16,24 @@ import org.springframework.stereotype.Service;
 public class DeliveryService {
     @Autowired
     private DeliveryRepository deliveryRepository;
-
     @Autowired
     private OrderRepository orderRepository;
-
     @Autowired
     private DeliveryDriverRepository deliveryDriverRepository;
+
+    //assignDriverToOrder
+    public DeliveryResponseDTO assignDriverToOrder(Integer orderId, Integer driverId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found"));
+
+        DeliveryDriver driver = deliveryDriverRepository.findById(driverId)
+                        .orElseThrow(() -> new ResourceNotFoundException("Driver not found"));
+        Delivery delivery = new Delivery();
+        delivery.setOrder(order);
+        delivery.setDeliveryDriver(driver);
+        delivery.setTrackingCode(HelperUtils.generateCode("TRK"));
+        delivery.setStatus("ASSIGNED");
+        deliveryRepository.save(delivery);
+        return DeliveryResponseDTO.fromEntity(delivery);
+    }
 }
