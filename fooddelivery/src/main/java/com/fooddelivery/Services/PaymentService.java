7c463123782047ea3a1873dbea323b7c751a60b2
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-02 08:34:49 +0400

    CREATE processPayment METHOD

--- fooddelivery/src/main/java/com/fooddelivery/Services/PaymentService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/PaymentService.java
@@ -1,9 +1,16 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.Dto.PaymentRequestDTO;
+import com.fooddelivery.Dto.PaymentResponseDTO;
+import com.fooddelivery.Entities.Order;
+import com.fooddelivery.Entities.Payment;
+import com.fooddelivery.Exceptions.ResourceNotFoundException;
 import com.fooddelivery.Repositories.OrderRepository;
 import com.fooddelivery.Repositories.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class PaymentService {
@@ -11,4 +18,15 @@ public class PaymentService {
     private PaymentRepository paymentRepository;
     @Autowired
     private OrderRepository orderRepository;
+
+    public PaymentResponseDTO processPayment(PaymentRequestDTO dto) {
+
+        Order order = orderRepository.findById(dto.getOrderId())
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found"));
+        Payment payment = dto.toEntity();
+        payment.setOrder(order);
+        payment.setProcessedAt(LocalDateTime.now().toString());
+        paymentRepository.save(payment);
+        return PaymentResponseDTO.fromEntity(payment);
+    }
 }
