fab949c438cd3884e033f54e31e014395409294d
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-02 08:43:15 +0400

    @Autowired

--- fooddelivery/src/main/java/com/fooddelivery/Services/ReviewService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/ReviewService.java
@@ -1,7 +1,20 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.Repositories.CustomerRepository;
+import com.fooddelivery.Repositories.DeliveryDriverRepository;
+import com.fooddelivery.Repositories.RestaurantRepository;
+import com.fooddelivery.Repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+    @Autowired
+    private DeliveryDriverRepository deliveryDriverRepository;
 }
