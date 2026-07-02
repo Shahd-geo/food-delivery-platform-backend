7568d392f1e01b02647a8c3fb960ab86b0c47d55
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-02 08:38:19 +0400

    ADD QUARY

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/PaymentRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/PaymentRepository.java
@@ -2,8 +2,19 @@ package com.fooddelivery.Repositories;
 
 import com.fooddelivery.Entities.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PaymentRepository  extends JpaRepository<Payment,Integer> {
+    @Query("""
+        SELECT p
+        FROM Payment p
+        WHERE p.order.id = :orderId
+        AND p.isActive = true
+        """)
+    Optional<Payment> findByOrderId(@Param("orderId") Integer orderId);
 }
